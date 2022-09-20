56ba8ea25c7e85a90600272be937eef2a794c3ad
Дима <hitrov21@gmail.com>
2022-09-20 12:16:45 +0300

    ex002

---
+++ ex002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ВВедите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int ost = a % 2;
+if (ost == 0)
+{
+    Console.WriteLine("Четное число!");
+}
+else
+{
+   Console.WriteLine("Не Четное число!"); 
+}
