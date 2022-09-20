94da967c276e71811065c5b1f671d858a482f441
Дима <hitrov21@gmail.com>
2022-09-20 13:07:31 +0300

    ex004

---
+++ ex004/Program.cs
@@ -0,0 +1,21 @@
+﻿// задача 4
+
+Console.Clear();
+
+Console.Write("Enter number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number b: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number c: ");
+int c = int.Parse(Console.ReadLine());
+
+int numberMax = int.MinValue;
+numberMax = numberMax > a ? numberMax : a;
+if (numberMax > a) numberMax = numberMax;
+else numberMax = a;
+numberMax = numberMax > b ? numberMax : b;
+numberMax = numberMax > c ? numberMax : c;
+
+Console.WriteLine(("Maximum number is ") + (numberMax));
