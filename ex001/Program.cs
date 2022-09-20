1fd2000e112dd0d36c682c60d936f2fe1d0d2b35
Дима <hitrov21@gmail.com>
2022-09-20 12:04:49 +0300

    ex001

---
+++ ex001/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberOne = 0;
+int numberTwo = 0;
+while (true){
+    try{
+        Console.Write("Введите первое число: ");
+        numberOne = int.Parse(Console.ReadLine() ?? "0");
+        Console.Write("Введите второе число: ");
+        numberTwo = int.Parse(Console.ReadLine() ?? "0");
+        break;
+    }
+    catch
+    {
+        Console.Write("Введите некорректное значение,введите целое число: ");  
+    }
+}
+
+if (numberOne > numberTwo) 
+    Console.Write("Число {0} больше числа {1}", numberOne, numberTwo);
+else if (numberOne == numberTwo) Console.Write("Число {0} больше числа {1}", numberOne, numberTwo);
+else Console.Write("Число {1} больше числа {0}", numberOne, numberTwo);
